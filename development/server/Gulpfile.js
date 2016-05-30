87bbf33c4dd31dec9234148310e286a468c151e6
Robert Kettler <kettler.robert@gmail.com>
2016-05-30 18:23:58 +0200

    Fix the coverage not displaying all files correctly.

--- development/server/Gulpfile.js
+++ development/server/Gulpfile.js
@@ -1,27 +1,30 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
+var print = require('gulp-print');
 
 gulp.task('default', ['mocha'], function() {
-      gulp.watch('**/*.js', {interval: 1000, mode: 'poll'}, ['mocha']);
+      gulp.watch('**/*.js', '!node_modules/**', '!coverage/**', {interval: 1000, mode: 'poll'}, ['mocha']);
 });
 
 gulp.task('mocha', function() {
-    return gulp.src(['**/*.spec.js'], { read: false })
+    return gulp.src(['**/*.spec.js', '!node_modules/**', '!coverage/**'], { read: false })
         .pipe(mocha({ reporter: 'Spec' }));
 });
 
 gulp.task('jenkins', ['jenkins-test']);
 
 gulp.task('pre-test', function () {
-  return gulp.src(['**/*.js', '!**/*.spec.js'])
+  return gulp.src(['**/!(*.spec).js', '!node_modules/**', '!coverage/**'])
+    .pipe(print())
     .pipe(istanbul())
     .pipe(istanbul.hookRequire());
 });
 
-gulp.task('jenkins-test', function() {
-    return gulp.src(['**/*.spec.js'], { read: false })
-        .pipe(mocha({ reporter: 'tap' }))
-	.pipe(istanbul.writeReports());
+gulp.task('jenkins-test', ['pre-test'], function() {
+    return gulp.src(['**/*.spec.js', '!node_modules/**', '!coverage/**'])
+       .pipe(print())
+       .pipe(mocha())
+	   .pipe(istanbul.writeReports());
 });
 
