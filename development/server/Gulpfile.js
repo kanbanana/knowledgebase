98a56a415085d563c390b763171d3aa1e460398f
Robert Kettler <kettler.robert@gmail.com>
2016-05-30 14:58:38 +0200

    Add jenkinsjob to gulpfile for coverage

--- development/server/Gulpfile.js
+++ development/server/Gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
 
 gulp.task('default', ['mocha'], function() {
       gulp.watch('**/*.js', {interval: 1000, mode: 'poll'}, ['mocha']);
@@ -9,3 +10,18 @@ gulp.task('mocha', function() {
     return gulp.src(['**/*.spec.js'], { read: false })
         .pipe(mocha({ reporter: 'Spec' }));
 });
+
+gulp.task('jenkins', ['jenkins-test']);
+
+gulp.task('pre-test', function () {
+  return gulp.src(['**/*.js', '!**/*.spec.js'])
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('jenkins-test', function() {
+    return gulp.src(['**/*.spec.js'], { read: false })
+        .pipe(mocha({ reporter: 'tap' }))
+	.pipe(istanbul.writeReports());
+});
+
