9e2cc0ff6b27ba91d438724624bfa114532ec144
Robert Kettler <kettler.robert@gmail.com>
2016-06-04 14:04:09 +0200

    Move server logic into server.js. Added listen and close method to be able to start and stop the server from within tests

--- development/server/server.spec.js
+++ development/server/server.spec.js
@@ -1,5 +1,17 @@
 var assert = require('assert');
-var app = require(__dirname + '/server.js');
+var request = require('supertest');
+
+var application = require(__dirname + '/server.js');
+
+describe('server', function () {
+    before(function () {
+        application.listen();
+    });
+
+    after(function () {
+        application.close();
+    });
+});
 
 describe('Array', function () {
     describe('#indexOf()', function () {
@@ -11,8 +23,17 @@ describe('Array', function () {
 
     describe('#indexOf()', function () {
         it('should return -1 when the value is not present', function () {
-            assert.equal(-1, [1, 2, 3].indexOf(5)); 
-            //assert.equal(80, app.server.address().port);
+            assert.equal(-1, [1, 2, 3].indexOf(5));
+        });
+    });
+
+    describe('GET /', function() {
+        it('respond with json', function(done) {
+            request(application.app)
+                .get('/')
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /html/)
+                .expect(200, done);
         });
     });
 });
