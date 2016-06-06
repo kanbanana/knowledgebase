ae6f189e09a3f6d790ca4d1684d0dcfb90c0884e
Robert Kettler <kettler.robert@gmail.com>
2016-06-06 14:22:17 +0200

    Add error output to testreport.

--- development/server/server.spec.js
+++ development/server/server.spec.js
@@ -41,6 +41,7 @@ describe('server', function () {
             it('respond with json', function(done) {
                 this.timeout(15000);
                 exec('node node_modules/.bin/abao ./raml/api.raml --server http://localhost:3000/api', function (err, stdout) {
+                    console.log(err);
                     console.log(stdout);
                     done();
                 });
