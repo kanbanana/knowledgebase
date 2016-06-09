81743af02e997f47bfcfa8b4804dce661666d25d
Timo Notheisen <timo.notheisen@googlemail.com>
2016-06-09 13:02:13 +0200

    cleaned up articles route

--- development/server/lib/routes/articles.js
+++ development/server/lib/routes/articles.js
@@ -6,82 +6,56 @@ var config = require('../config/config');
 
 var router = module.exports = {};
 
-router.middlewareRetrieveArticle = function(req, res, next) {
-    articleService.findArticleById(req.params.articleId).then(function(article) {
-        req.article = article;
-        next();
-    }, function(err){
+// ********************* Route Handlers *********************  //
+
+router.onArticleCreateHandler = function (req, res) {
+    articleService.createArticle().then(function (article) {
+        res.send(article._id);
+    }, function (error) {
         res.send(500, error);
     });
 };
 
-router.onDocumentUploadHandler = function(req, res) {
-    articleService.saveDocuments(req.article, req.files).then(function(storageInfoList) {
+router.onDocumentUploadHandler = function (req, res) {
+    articleService.saveDocuments(req.article, req.files).then(function (storageInfoList) {
         res.send(storageInfoList);
-    }, function(error) {
+    }, function (error) {
         res.send(500, error);
     })
 };
 
-router.onArticleCreateHandler = function(req, res) {
-    articleService.createArticle().then(function(article) {
-        res.send(article._id);
-    }, function(error) {
-        res.send(500, error);
-    });
-};
-
-router.middlewareCeckTitle = function(req, res, next) {
-    if(req.body.title && req.body.title.length > config.postBodyValidationValues.maxArticleTitleLength) {
-        return res.status(400).send('Invalid title length (max. ' + config.postBodyValidationValues.maxArticleTitleLength + ' characters).');
-    }
-
-    next();
-};
-
-router.middlewareCeckAutherName = function(req, res, next) {
-    if(req.body.author && req.body.author.name && req.body.author.name.length > config.postBodyValidationValues.maxArticleAuthorNameLength ||
-        req.body.lastChangedBy && req.body.lastChangedBy.name && req.body.lastChangedBy.name.length > config.postBodyValidationValues.maxArticleAuthorNameLength) {
-        return res.status(400).send('Invalid author name length (max. ' + config.postBodyValidationValues.maxArticleAuthorNameLength + ' characters).');
-    }
-
-    next();
-};
-
-router.middlewareCeckAutherMail = function(req, res, next) {
-    if(req.body.author && req.body.author.email && req.body.author.email.length > config.postBodyValidationValues.maxArticleAuthorEmailLength ||
-        req.body.lastChangedBy && req.body.lastChangedBy.email && req.body.lastChangedBy.email.length > config.postBodyValidationValues.maxArticleAuthorEmailLength) {
-        return res.status(400).send('Invalid author email address length (max. ' + config.postBodyValidationValues.maxArticleAuthorEmailLength + ' characters).');
-    }
-
-    next()
-};
-
-router.onArticleSaveHandler = function(req, res) {
-    articleService.saveArticle(req.article, req.body.title, req.body.text, req.body.lastChangedBy).then(function(article) {
-        articleService.getArticleContent(article._id).then(function(articleContent) {
+router.onArticleSaveHandler = function (req, res) {
+    articleService.saveArticle(req.article, req.body.title, req.body.text, req.body.lastChangedBy).then(function (article) {
+        articleService.getArticleContent(article._id).then(function (articleContent) {
             var responseArticle = models.articleSchemaToResponseArticle(article);
             responseArticle.text = articleContent;
             res.send(responseArticle);
         });
-    }, function(error) {
+    }, function (error) {
         res.status(500).send(error);
     });
 };
 
-router.onArticleGetHandler = function(req, res) {
-    if(!req.article) {
+router.onArticleGetHandler = function (req, res) {
+    if (!req.article) {
         return res.status(404).send('Article not found');
     }
-    articleService.getArticleContent(req.article._id).then(function(articleContent) {
+    if (req.query.old === '') {
+        return articleService.getOldArticleContentAndTitle(req.article._id).then(function (contentAndTitle) {
+            res.send(contentAndTitle);
+        }, function (err) {
+            res.status(500).send("Error while reading old version.");
+        });
+    }
+    articleService.getArticleContent(req.article._id).then(function (articleContent) {
         var responseArticle = models.articleSchemaToResponseArticle(req.article);
         responseArticle.text = articleContent;
         res.send(responseArticle);
     });
 };
 
-router.onArticleSearchHandler = function(req, res) {
-    if(req.query.q) {
+router.onArticleSearchHandler = function (req, res) {
+    if (req.query.q) {
         articleService.searchArticles(req.query.q).then(function (searchResults) {
             res.send(models.multipleArticleSchemaToResponseArticles(searchResults));
         }, function (error) {
@@ -92,18 +66,55 @@ router.onArticleSearchHandler = function(req, res) {
     }
 };
 
-router.onArticleDeleteHandler = function(req,res){
-    articleService.deleteArticle(req.article._id).then(function() {
+router.onArticleDeleteHandler = function (req, res) {
+    articleService.deleteArticle(req.article._id).then(function () {
         res.send(true);
-    }, function(err){
+    }, function (err) {
         res.status(500).send(err);
     });
 };
 
-router.onDocumentDeleteHandler = function(req,res){
-    articleService.deleteDocument(req.article, req.params.filename).then(function() {
+router.onDocumentDeleteHandler = function (req, res) {
+    articleService.deleteDocument(req.article, req.params.filename).then(function () {
         res.send(true);
-    }, function(err){
+    }, function (err) {
         res.status(500).send(err);
     });
+};
+
+// ********************* Middlewares *********************  //
+
+router.middlewareRetrieveArticle = function (req, res, next) {
+    articleService.findArticleById(req.params.articleId).then(function (article) {
+        req.article = article;
+        next();
+    }, function (err) {
+        res.send(500, error);
+    });
+};
+
+router.middlewareCheckTitle = function (req, res, next) {
+    if (req.body.title && req.body.title.length > config.postBodyValidationValues.maxArticleTitleLength) {
+        return res.status(400).send('Invalid title length (max. ' + config.postBodyValidationValues.maxArticleTitleLength + ' characters).');
+    }
+
+    next();
+};
+
+router.middlewareCheckAutherName = function (req, res, next) {
+    if (req.body.author && req.body.author.name && req.body.author.name.length > config.postBodyValidationValues.maxArticleAuthorNameLength ||
+        req.body.lastChangedBy && req.body.lastChangedBy.name && req.body.lastChangedBy.name.length > config.postBodyValidationValues.maxArticleAuthorNameLength) {
+        return res.status(400).send('Invalid author name length (max. ' + config.postBodyValidationValues.maxArticleAuthorNameLength + ' characters).');
+    }
+
+    next();
+};
+
+router.middlewareCheckAutherMail = function (req, res, next) {
+    if (req.body.author && req.body.author.email && req.body.author.email.length > config.postBodyValidationValues.maxArticleAuthorEmailLength ||
+        req.body.lastChangedBy && req.body.lastChangedBy.email && req.body.lastChangedBy.email.length > config.postBodyValidationValues.maxArticleAuthorEmailLength) {
+        return res.status(400).send('Invalid author email address length (max. ' + config.postBodyValidationValues.maxArticleAuthorEmailLength + ' characters).');
+    }
+
+    next()
 };
