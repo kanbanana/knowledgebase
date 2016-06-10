760b83fae8a6219fd4c7bf293bffed6cd6c55578
Robert Kettler <kettler.robert@gmail.com>
2016-06-10 14:55:43 +0200

    Change path to absurl.

--- development/client/public/modules/core/main.js
+++ development/client/public/modules/core/main.js
@@ -3,7 +3,8 @@
 angular.module('core', [])
     .run(['$rootScope','$location', function($rootScope, $location) {
         $rootScope.bodyClass = 'loading';
-        var url = $location.path();
+        //var url = $location.path();
+        var url = $location.absUrl();
         //var url = $location.host();
         //var url = "http://141.19.158.68:3000/#/";
         var cutUrl = url.substring(0, url.indexOf('/#/') );
