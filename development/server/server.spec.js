6e85f0c889dbd80f93e3b49d20c7f073bacbdc3b
Robert Kettler <kettler.robert@gmail.com>
2016-06-11 15:42:24 +0200

    removed dummy tests. added "use strict".

--- development/server/server.spec.js
+++ development/server/server.spec.js
@@ -1,3 +1,4 @@
+"use strict";
 var assert = require('assert');
 var request = require('supertest');
 var exec = require('child_process').exec;
@@ -13,39 +14,24 @@ describe('server', function () {
         application.close();
     });
 
-    describe('Array', function () {
-        describe('#indexOf()', function () {
-            it('should return -1 when the value is not present', function () {
-                assert.equal(-1, [1, 2, 3].indexOf(5));
-                assert.equal(-1, [1, 2, 3].indexOf(0));
-            });
-        });
-
-        describe('#indexOf()', function () {
-            it('should return -1 when the value is not present', function () {
-                assert.equal(-1, [1, 2, 3].indexOf(5));
-            });
-        });
-
-        describe('GET /', function() {
-            it('respond with json', function(done) {
-                request(application.app)
-                    .get('/')
-                    .set('Accept', 'application/json')
-                    .expect('Content-Type', /html/)
-                    .expect(200, done);
-            });
-        });
-
-        describe('Run Abao', function() {
-            it('respond with json', function(done) {
-                this.timeout(15000);
-                exec('node node_modules/abao/bin/abao ./raml/api.raml --server http://localhost:3000/api', function (err, stdout) {
-                    console.log(err);
-                    console.log(stdout);
-                    done();
-                });
-            });
+    describe('GET /api/articles/q=test', function () {
+        it('respond with json', function (done) {
+            request(application.app)
+                .get('/api/articles/q=test')
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /html/)
+                .expect(200, done);
         });
     });
+
+    // describe('Run Abao', function () {
+    //     it('respond with json', function (done) {
+    //         this.timeout(15000);
+    //         exec('node node_modules/abao/bin/abao ./raml/api.raml --server http://localhost:3000/api', function (err, stdout) {
+    //             console.log(err);
+    //             console.log(stdout);
+    //             done();
+    //         });
+    //     });
+    // });
 });
