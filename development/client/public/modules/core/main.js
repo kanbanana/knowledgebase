b7f22be0d28929b61074512027565a127198e58f
Christoph Brutscher <christoph.brutscher@sovanta.com>
2016-06-13 15:13:23 +0200

    Added '...' too big file names Fixed url?

--- development/client/public/modules/core/main.js
+++ development/client/public/modules/core/main.js
@@ -3,10 +3,14 @@
 angular.module('core', [])
     .run(['$rootScope','$location', function($rootScope, $location) {
         $rootScope.bodyClass = 'loading';
-        //var url = $location.path();
-        //var url = $location.host();
-        var url = "http://141.19.158.68:3000/#/";
-        var cutUrl = url.substring(0, url.indexOf('/#/') );
+        var url = $location.absUrl();
+        //var url = "http://141.19.158.68:3000/#/";
+        if (url.indexOf('/#/') > -1) {
+            var cutUrl = url.substring(0, url.indexOf('/#/'));
+        } else {
+            var cutUrl = url;
+        }
+
         //var cutUrl = "https://danielweidle.de"
         $rootScope.baseUrl = cutUrl;
     }]);
