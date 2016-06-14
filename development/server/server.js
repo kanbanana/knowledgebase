8f63914f34542d64b2b75d7d74f9eb806dcee3cb
Timo <timo.notheisen@gmail.com>
2016-06-14 16:34:23 +0200

    removed unnecessary commented code

--- development/server/server.js
+++ development/server/server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var logger = require('morgan');
-// var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var debug = require('debug')('server');
 var config = require('./lib/config/config');
@@ -19,7 +18,6 @@ app.set('port', config.port);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-// app.use(cookieParser());
 
 // Define Routes
 router.all('/:articleId*',articles.middlewareRetrieveArticle);
